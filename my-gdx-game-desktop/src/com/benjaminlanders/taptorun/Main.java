9672bead279bb8d70b19e4231fc2140ad5bc8d10
Benjamin Landers <benjaminrlanders@gmail.com>
2013-04-26 19:42:52 -0700

    startup libgdx

---
+++ my-gdx-game-desktop/src/com/benjaminlanders/taptorun/Main.java
@@ -0,0 +1,16 @@
+package com.benjaminlanders.taptorun;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "my-gdx-game";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new TapToRunMain(), cfg);
+	}
+}
